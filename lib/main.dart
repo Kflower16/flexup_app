26ffe8044377c9f94a778759a77bfa6ca855ff8e
Kflower16 <khoale161616@gmail.com>
2025-11-23 00:45:00 +0700

    feat: update stories UI with viewed state and add new story option

--- lib/main.dart
+++ lib/main.dart
@@ -40,9 +40,16 @@ class HomeScreen extends StatelessWidget{
       children: [
         //composer
         _StatusComposer(),
+        
+        Container(color: Colors.grey[400],height: 3.4,),
 
         //stories
-        _StoriesSection(),
+        Padding(
+          padding: const EdgeInsets.only(top: 4.5, bottom: 4.5),
+          child: _StoriesSection(),
+        ),
+
+        Container(color: Colors.grey[400],height: 3.4,),
       ],
     ),
     bottomNavigationBar: _FacebookBottomNav(),
@@ -83,15 +90,82 @@ class _StoriesSection extends StatelessWidget{
         scrollDirection: Axis.horizontal,
         children: [
           SizedBox(width: 3,),
-          _StoryItem(imagaPath: 'assets/images/img_ava_1.jpg',userName: 'Mỹ Tiên', userAvatarPath: 'assets/images/img_ava_1.jpg', userOnline: false,),
-          _StoryItem(imagaPath: 'assets/images/img_ava_2.jpg',userName: 'Hồng Ngọc', userAvatarPath: 'assets/images/img_ava_2.jpg',userOnline: true,),
-          _StoryItem(imagaPath: 'assets/images/img_ava_3.jpg',userName: 'Trúc Lan', userAvatarPath: 'assets/images/img_ava_3.jpg',userOnline: false,),
-          _StoryItem(imagaPath: 'assets/images/img_ava_2.jpg',userName: 'Gia Hân', userAvatarPath: 'assets/images/img_ava_2.jpg',userOnline: true,),
-          _StoryItem(imagaPath: 'assets/images/img_ava_3.jpg',userName: 'Ánh Nguyệt', userAvatarPath: 'assets/images/img_ava_3.jpg',userOnline: false,),
-          _StoryItem(imagaPath: 'assets/images/img_ava_1.jpg',userName: 'Ngọc Bảo', userAvatarPath: 'assets/images/img_ava_1.jpg',userOnline: true,),
+          _MyStoryItem(mainUserAva: 'assets/images/img_ava_2.jpg',),
+          _StoryItem(imagaPath: 'assets/images/img_ava_1.jpg',userName: 'Mỹ Tiên', userAvatarPath: 'assets/images/img_ava_1.jpg', isUserOnline: false, isStorySeen: false,),
+          _StoryItem(imagaPath: 'assets/images/img_ava_2.jpg',userName: 'Hồng Ngọc', userAvatarPath: 'assets/images/img_ava_2.jpg',isUserOnline: true, isStorySeen: true,),
+          _StoryItem(imagaPath: 'assets/images/img_ava_3.jpg',userName: 'Trúc Lan', userAvatarPath: 'assets/images/img_ava_3.jpg',isUserOnline: false, isStorySeen: true),
+          _StoryItem(imagaPath: 'assets/images/img_ava_2.jpg',userName: 'Gia Hân', userAvatarPath: 'assets/images/img_ava_2.jpg',isUserOnline: true, isStorySeen: false),
+          _StoryItem(imagaPath: 'assets/images/img_ava_3.jpg',userName: 'Ánh Nguyệt', userAvatarPath: 'assets/images/img_ava_3.jpg',isUserOnline: false, isStorySeen: false),
+          _StoryItem(imagaPath: 'assets/images/img_ava_1.jpg',userName: 'Ngọc Bảo', userAvatarPath: 'assets/images/img_ava_1.jpg',isUserOnline: true, isStorySeen: false),
         ],
       ),
     );
+  }
+
+}
+
+class _MyStoryItem extends StatelessWidget{
+  final String mainUserAva;
+
+  const _MyStoryItem({required this.mainUserAva});
+
+  @override
+  Widget build(BuildContext context) {
+   return Container(
+    width: 118,
+    margin: EdgeInsets.only(right: 5),
+    decoration: BoxDecoration(
+      color: Color(0xddf8f9fb),
+      borderRadius: BorderRadius.circular(12),
+      border: Border.all(width: 0.5, color: Color(0xddcbcdd2))
+    ),
+    child: Column(
+      children: [
+        Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Container(
+                width: double.infinity, // Chiếm hết chiều rộng
+                height: 200*0.7, // Chiều cao cố định
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(mainUserAva),
+                    fit: BoxFit.cover
+                  ),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(12),
+                    topRight: Radius.circular(12),
+                  ),
+                ),
+            ),
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: -12,
+              child: Container(
+                padding: const EdgeInsets.all(2),
+                decoration: BoxDecoration(
+                      color: Color(0xFF005aff), // Màu nền xanh dương cho dấu cộng
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.white, width: 2.5), // Viền trắng
+                ),
+                child: const Icon(
+                  Icons.add,
+                  color: Colors.white,
+                  size: 20,
+                ),
+              ),
+            ),
+          ],
+        ),
+        const Spacer(),
+        Padding(
+          padding: const EdgeInsets.only(bottom: 3.2),
+          child: const Text('Tạo tin',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 12),),
+        )
+      ],
+    ),
+   );
   }
 
 }
@@ -100,15 +174,16 @@ class _StoryItem extends StatelessWidget{
   final String imagaPath;
   final String userName;
   final String userAvatarPath;
-  final bool userOnline;
+  final bool isUserOnline;
+  final bool isStorySeen;
 
-  const _StoryItem({required this.imagaPath, required this.userAvatarPath, required this.userName, required this.userOnline});
+  const _StoryItem({required this.imagaPath, required this.userAvatarPath, required this.userName, required this.isUserOnline, required this.isStorySeen});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 118,
-      margin: EdgeInsets.only(right: 8),
+      margin: EdgeInsets.only(right: 5),
       decoration: BoxDecoration(
         color: Colors.grey[300],
         image: DecorationImage(
@@ -116,7 +191,8 @@ class _StoryItem extends StatelessWidget{
           image: AssetImage(imagaPath
           )
         ),
-        borderRadius: BorderRadius.circular(12)
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(width: 0.5, color: Color(0xffcbcdd2))
       ),
       child: Padding(
         padding: const EdgeInsets.all(8.0),
@@ -132,8 +208,8 @@ class _StoryItem extends StatelessWidget{
                   decoration: BoxDecoration(
                     shape: BoxShape.circle,
                     border: Border.all(
-                      color: const Color(0xFF005aff),
-                      width: 3,
+                      color: isStorySeen? Colors.white:const Color(0xFF005aff),
+                      width: isStorySeen? 0.75: 1.7,
                     )
                   ),
                   child: CircleAvatar(
@@ -141,14 +217,15 @@ class _StoryItem extends StatelessWidget{
                     radius: 12,
                   )
                 ),
-                userOnline?
+                isUserOnline?
                 Positioned(
                   bottom: 0,
                   right: 0,
                   child: Container(
                     width: 12.3,
                     height: 12.3,
-                    decoration: BoxDecoration(
+                    decoration: 
+                    BoxDecoration(
                       color: Colors.green,
                       shape: BoxShape.circle,
                       border: Border.all(
